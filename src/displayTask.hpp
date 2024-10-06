47fa3232134edd045177da200d225e1ca01ed1fc
Cellie <CelliesProjects@users.noreply.github.com>
2024-10-06 15:26:29 +0200

    Added a weather update task
    Embedded weather icons from https://github.com/visualcrossing/WeatherIcons

--- src/displayTask.hpp
+++ src/displayTask.hpp
@@ -28,4 +28,73 @@ QueueHandle_t displayQueue = nullptr;
 
 void displayTask(void *parameter);
 
+/* weather icons from https://github.com/visualcrossing/WeatherIcons/tree/main/PNG/1st%20Set%20-%20Color */
+
+extern const uint8_t clear_day_png_start[] asm("_binary_clear_day_png_start");
+extern const uint8_t clear_day_png_end[] asm("_binary_clear_day_png_end");
+
+extern const uint8_t clear_night_png_start[] asm("_binary_clear_night_png_start");
+extern const uint8_t clear_night_png_end[] asm("_binary_clear_night_png_end");
+
+extern const uint8_t cloudy_png_start[] asm("_binary_cloudy_png_start");
+extern const uint8_t cloudy_png_end[] asm("_binary_cloudy_png_end");
+
+extern const uint8_t fog_png_start[] asm("_binary_fog_png_start");
+extern const uint8_t fog_png_end[] asm("_binary_fog_png_end");
+
+extern const uint8_t hail_png_start[] asm("_binary_hail_png_start");
+extern const uint8_t hail_png_end[] asm("_binary_hail_png_end");
+
+extern const uint8_t partly_cloudy_day_png_start[] asm("_binary_partly_cloudy_day_png_start");
+extern const uint8_t partly_cloudy_day_png_end[] asm("_binary_partly_cloudy_day_png_end");
+
+extern const uint8_t partly_cloudy_night_png_start[] asm("_binary_partly_cloudy_night_png_start");
+extern const uint8_t partly_cloudy_night_png_end[] asm("_binary_partly_cloudy_night_png_end");
+
+extern const uint8_t rain_png_start[] asm("_binary_rain_png_start");
+extern const uint8_t rain_png_end[] asm("_binary_rain_png_end");
+
+extern const uint8_t rain_snow_png_start[] asm("_binary_rain_snow_png_start");
+extern const uint8_t rain_snow_png_end[] asm("_binary_rain_snow_png_end");
+
+extern const uint8_t rain_snow_showers_day_png_start[] asm("_binary_rain_snow_showers_day_png_start");
+extern const uint8_t rain_snow_showers_day_png_end[] asm("_binary_rain_snow_showers_day_png_end");
+
+extern const uint8_t rain_snow_showers_night_png_start[] asm("_binary_rain_snow_showers_night_png_start");
+extern const uint8_t rain_snow_showers_night_png_end[] asm("_binary_rain_snow_showers_night_png_end");
+
+extern const uint8_t showers_day_png_start[] asm("_binary_showers_day_png_start");
+extern const uint8_t showers_day_png_end[] asm("_binary_showers_day_png_end");
+
+extern const uint8_t showers_night_png_start[] asm("_binary_showers_night_png_start");
+extern const uint8_t showers_night_png_end[] asm("_binary_showers_night_png_end");
+
+extern const uint8_t sleet_png_start[] asm("_binary_sleet_png_start");
+extern const uint8_t sleet_png_end[] asm("_binary_sleet_png_end");
+
+extern const uint8_t snow_png_start[] asm("_binary_snow_png_start");
+extern const uint8_t snow_png_end[] asm("_binary_snow_png_end");
+
+extern const uint8_t snow_showers_day_png_start[] asm("_binary_snow_showers_day_png_start");
+extern const uint8_t snow_showers_day_png_end[] asm("_binary_snow_showers_day_png_end");
+
+extern const uint8_t snow_showers_night_png_start[] asm("_binary_snow_showers_night_png_start");
+extern const uint8_t snow_showers_night_png_end[] asm("_binary_snow_showers_night_png_end");
+
+extern const uint8_t thunder_png_start[] asm("_binary_thunder_png_start");
+extern const uint8_t thunder_png_end[] asm("_binary_thunder_png_end");
+
+extern const uint8_t thunder_rain_png_start[] asm("_binary_thunder_rain_png_start");
+extern const uint8_t thunder_rain_png_end[] asm("_binary_thunder_rain_png_end");
+
+extern const uint8_t thunder_showers_day_png_start[] asm("_binary_thunder_showers_day_png_start");
+extern const uint8_t thunder_showers_day_png_end[] asm("_binary_thunder_showers_day_png_end");
+
+extern const uint8_t thunder_showers_night_png_start[] asm("_binary_thunder_showers_night_png_start");
+extern const uint8_t thunder_showers_night_png_end[] asm("_binary_thunder_showers_night_png_end");
+
+extern const uint8_t wind_png_start[] asm("_binary_wind_png_start");
+extern const uint8_t wind_png_end[] asm("_binary_wind_png_end");
+
+
 #endif
--- src/weatherdataTask.cpp
+++ src/weatherdataTask.cpp
@@ -10,26 +10,27 @@ void getWeatherDataTask(void *parameter)
         vTaskDelete(NULL);
     }
 
-    // Go to https://www.visualcrossing.com/weather/weather-data-services/ to compose a sample query
-
-    String url;
-    url.reserve(512);
-    url.concat("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/");
-    url.concat(VISUAL_CROSSING_CITY);
-    url.concat(",%20");
-    url.concat(VISUAL_CROSSING_COUNTRY);
-    url.concat("?unitGroup=metric&elements=datetime,temp,description,conditions,icon&include=days%2Cobs&key=");
-    url.concat(VISUAL_CROSSING_API_KEY);
-    url.concat("&contentType=json");
-
     WiFiClientSecure client;
-    client.setInsecure();
+    client.setInsecure(); // TODO: add root certs
 
     HTTPClient http;
-    if (!http.begin(client, url))
     {
-        log_e("could not reach %s", url.c_str());
-        vTaskDelete(NULL);
+        // See https://www.visualcrossing.com/weather/weather-data-services/ to compose a sample query
+        String url;
+        url.reserve(512);
+        url.concat("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/");
+        url.concat(VISUAL_CROSSING_CITY);
+        url.concat(",%20");
+        url.concat(VISUAL_CROSSING_COUNTRY);
+        url.concat("?unitGroup=metric&elements=datetime,temp,description,conditions,icon&include=days%2Cobs&key=");
+        url.concat(VISUAL_CROSSING_API_KEY);
+        url.concat("&contentType=json");
+
+        if (!http.begin(client, url))
+        {
+            log_e("could not reach %s", url.c_str());
+            vTaskDelete(NULL);
+        }
     }
 
     const auto RESPONSE_CODE = http.GET();
@@ -63,8 +64,6 @@ void getWeatherDataTask(void *parameter)
     msg.floatVal = doc["days"][0]["temp"];
     msg.type = displayMessage::WEATHER_UPDATE;
     xQueueSend(displayQueue, &msg, portMAX_DELAY);
-
-    log_i("icon retrieved: %s", msg.str);
 
     vTaskDelete(NULL);
 }
