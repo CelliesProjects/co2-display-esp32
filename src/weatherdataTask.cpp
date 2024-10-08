ee341352faae800dfb1dcd63a2e96b1d63b8f5c2
Cellie <CelliesProjects@users.noreply.github.com>
2024-10-08 22:16:47 +0200

    Added code to test without using visulacrossing credits

--- src/weatherdataTask.cpp
+++ src/weatherdataTask.cpp
@@ -1,5 +1,33 @@
 #include "weatherDataTask.h"
 
+/* for debug: set to true to get values without using any visualcrossing credits */
+#define USE_RANDOM_GENERATED_VALUES false
+
+#if (USE_RANDOM_GENERATED_VALUES == true)
+static const char *getRandomWeatherString()
+{
+    // List of all possible weather strings
+    const char *weatherTypes[] = {
+        "clear-day", "clear-night", "cloudy", "fog", "hail",
+        "partly-cloudy-day", "partly-cloudy-night", "rain", "rain-snow",
+        "rain-snow-showers-day", "rain-snow-showers-night", "showers-day",
+        "showers-night", "sleet", "snow", "snow-showers-day",
+        "snow-showers-night", "thunder", "thunder-rain",
+        "thunder-showers-day", "thunder-showers-night", "wind"};
+
+    int numWeatherTypes = sizeof(weatherTypes) / sizeof(weatherTypes[0]);
+    randomSeed(esp_random()); // Use ESP32 random seed
+    int randomIndex = random(numWeatherTypes);
+    return weatherTypes[randomIndex];
+}
+
+static float randomFloat(float minVal, float maxVal)
+{
+    // Convert random integer to float by scaling and shifting
+    return minVal + (maxVal - minVal) * (random(10000) / 10000.0);
+}
+#endif
+
 // https://github.com/visualcrossing/WeatherApi/blob/master/Arduino_samples_esp32/src/sketch.ino
 
 void getWeatherDataTask(void *parameter)
@@ -9,6 +37,18 @@ void getWeatherDataTask(void *parameter)
         log_e("can not start weatherTask because of reasons");
         vTaskDelete(NULL);
     }
+
+#if (USE_RANDOM_GENERATED_VALUES == true)
+    {
+        displayMessage msg;
+        // const char *condition = doc["days"][0]["conditions"];
+        snprintf(msg.str, sizeof(msg.str), "%s", getRandomWeatherString());
+        msg.floatVal = randomFloat(-4, 34);
+        msg.type = displayMessage::WEATHER_UPDATE;
+        xQueueSend(displayQueue, &msg, portMAX_DELAY);
+        vTaskDelete(NULL);
+    }
+#endif
 
     WiFiClientSecure client;
     client.setInsecure(); // TODO: add root certs
@@ -22,9 +62,11 @@ void getWeatherDataTask(void *parameter)
         url.concat(VISUAL_CROSSING_CITY);
         url.concat(",%20");
         url.concat(VISUAL_CROSSING_COUNTRY);
-        url.concat("?unitGroup=metric&elements=datetime,temp,description,conditions,icon&include=days%2Cobs&key=");
+        url.concat("/today?unitGroup=metric&elements=datetime,temp,description,conditions,icon&include=days,obs&key=");
         url.concat(VISUAL_CROSSING_API_KEY);
         url.concat("&contentType=json");
+
+        log_v("request url: %s", url.c_str());
 
         if (!http.begin(client, url))
         {
